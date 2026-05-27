ad9d7c06b6d856278aa37b92ba418d92f17f4469
Gabby <gabriellelaah@gmail.com>
2026-05-26 21:34:55 -0500

    Initial Commit

---
+++ ZaHunter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZaHunter
+//
+//  Created by gabrielle on 5/26/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZaHunter/ZaHunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZaHunterApp.swift
+//  ZaHunter
+//
+//  Created by gabrielle on 5/26/26.
+//
+
+import SwiftUI
+
+@main
+struct ZaHunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
